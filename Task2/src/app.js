14154f5f7b04e410eac4780a352fd328bb061ed4
SiarheiBalt <jh-11@yandex.by>
2021-09-24 15:48:03 +0300

    fix: after review

--- Task2/src/app.js
+++ Task2/src/app.js
@@ -1,18 +1,35 @@
-const button = document.querySelector('.link_up');
-window.addEventListener('scroll', () => {
+const button = document.querySelector(".link_up");
+window.addEventListener("scroll", () => {
   button.hidden = scrollY < document.documentElement.clientHeight;
 });
 const anchors = document.querySelectorAll('a[href^="#"]');
 
 anchors.forEach((anchor) => {
-  anchor.addEventListener('click', (e) => {
+  anchor.addEventListener("click", (e) => {
     e.preventDefault();
-    const goto = anchor.hasAttribute('href')
-      ? anchor.getAttribute('href')
-      : 'body';
+    const goto = anchor.hasAttribute("href")
+      ? anchor.getAttribute("href")
+      : "body";
     document.querySelector(goto).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+      behavior: "smooth",
+      block: "start",
     });
   });
 });
+
+async function loadData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+const userData = loadData("https://jsonplaceholder.typicode.com/users")
+  .then((user) => {
+    const userEvenId = user.filter((el) => {
+      if (el.id % 2 === 0) return el;
+    });
+    return userEvenId;
+  })
+  .then((user) => {
+    console.log(user);
+  });
