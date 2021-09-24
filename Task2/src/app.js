b6bd99f1be36323b2a29ecea04def98d1fcc3715
SiarheiBalt <jh-11@yandex.by>
2021-09-24 12:36:59 +0300

    feat: install eslint

--- Task2/src/app.js
+++ Task2/src/app.js
@@ -1,19 +1,14 @@
 const button = document.querySelector(".link_up");
-
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
   button.hidden = scrollY < document.documentElement.clientHeight;
 });
-
 const anchors = document.querySelectorAll('a[href^="#"]');
-// Цикл по всем ссылкам
 for (let anchor of anchors) {
   anchor.addEventListener("click", function (e) {
-    e.preventDefault(); // Предотвратить стандартное поведение ссылок
-    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    e.preventDefault();
     const goto = anchor.hasAttribute("href")
       ? anchor.getAttribute("href")
       : "body";
-    // Плавная прокрутка до элемента с id = href у ссылки
     document.querySelector(goto).scrollIntoView({
       behavior: "smooth",
       block: "start",
